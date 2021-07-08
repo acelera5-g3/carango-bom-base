52c610ec6c1c3f53d9c80ce54bc6d6460efff53a
Diego Lopes <diego.lopes@thoughtworks.com>
2021-07-08 15:55:55 -0300

    ajustes em erros de compilação

--- src/pages/ListagemMarca/ListagemMarcas.jsx
+++ src/pages/ListagemMarca/ListagemMarcas.jsx
@@ -1,31 +1,13 @@
-import blue from '@material-ui/core/colors/blue';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import MarcaService from '../../services/Marca/MarcaService';
 import Listagem from '../../components/Listagem';
-import { makeStyles } from "@material-ui/core/styles";
 
 const colunas = [{ field: 'nome', headerName: 'Marca', width: 200 }];
-
-const useStyles = makeStyles(() => ({
-  actionsToolbar: {
-    float: 'right',
-  },
-  actions: {
-    top: '10px',
-    marginLeft: '10px',
-  },
-  dataTable: {
-    MuiDataGridColCellWrapper: {
-      background: blue[900],
-    },
-  },
-}));
 
 const ListagemMarcas = () => {
   const [marcas, setMarcas] = useState([]);
   const [marcaSelecionada, setMarcaSelecionada] = useState();
-  const classes = useStyles();
   const history = useHistory();
 
   function excluir() {
