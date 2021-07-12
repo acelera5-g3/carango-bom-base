19ce548240eb0b5a01f3e2397022e500ff393e75
Marcos Neto <marcos@MacBook-Pro-de-Marcos.local>
2021-07-12 14:49:39 -0300

    finalizacao das telas de cadastro de veiculo com testes

--- src/pages/Veiculos/CadastroVeiculos/CadastroVeiculos.test.js
+++ src/pages/Veiculos/CadastroVeiculos/CadastroVeiculos.test.js
@@ -2,83 +2,62 @@ import React from 'react';
 import { render, screen, fireEvent, act } from '@testing-library/react';
 import CadastroVeiculos from './CadastroVeiculos';
 import MarcaService from '../../../services/Marca/MarcaService';
+import { changeInput }  from '../../../tests/testing';
 
 describe('ListagemVeiculos', () => {
+
   it('Deve listar os veiculos', () => {
     const { container } = render(<CadastroVeiculos />);
     expect(container).toBeDefined();
   });
 
-  // it('Deve alterar o ano selecionado', async () => {
-  //   jest.spyOn(MarcaService, 'listar').mockResolvedValue([
-  //     {
-  //       id: 74,
-  //       marca: 'HONDA',
-  //     },
-  //     {
-  //       id: 4,
-  //       marca: 'HYUNDAI',
-  //     },
-  //   ]);
-  //   await act(async () => {
-  //     await render(<CadastroVeiculos />);
-  //   });
+  it('Verificar se as opções foram renderizadas', async () => {
+    jest.spyOn(MarcaService, 'listar').mockResolvedValue([
+      {
+        id: 74,
+        nome: 'HONDA',
+      },
+      {
+        id: 4,
+        nome: 'HYUNDAI',
+      },
+    ]);
 
-  //   const select = await screen.getByTestId('select-marca');
-  //   fireEvent.click(select);
-  //   const selectValue = await screen.findByText('HONDA');
-  //   fireEvent.click(selectValue);
+    await act(async () => {
+      await render(<CadastroVeiculos />);
+    });
 
-  //   expect(selectValue.value).toBe('HONDA');
-  // });
+    expect(screen.getByText("HONDA")).toBeInTheDocument();
+  });
 
-  // it('Deve mostrar lista com itens', async () => {
-  //   jest.spyOn(global, 'fetch').mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue([{
-  //       "id": 74,
-  //       "marca": "HONDA",
-  //       "modelo": "Honda Civic",
-  //       "ano": 2020,
-  //       "valor": "R$ 70.000,000"
-  //     }]),
-  //   });
-  //   render(<ListagemVeiculos />);
-  //   expect(await screen.findByText('HONDA')).toBeInTheDocument();
-  // });
+  it('Deve mudar os valores dos inputs', async () => {
+    jest.spyOn(MarcaService, 'listar').mockResolvedValue([
+      {
+        id: 74,
+        nome: 'HONDA',
+      },
+      {
+        id: 4,
+        nome: 'HYUNDAI',
+      },
+    ]);
 
-  // it('Deve excluir uma Veiculo', async () => {
-  //   jest.spyOn(global, 'fetch').mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue([
-  //       {
-  //         "id": 74,
-  //         "marca": "HONDA",
-  //         "modelo": "Honda Civic",
-  //         "ano": 2020,
-  //         "valor": "R$ 70.000,000"
-  //       },
-  //       {
-  //         "id": 4,
-  //         "marca": "HYUNDAY",
-  //         "modelo": "HB20",
-  //         "ano": 2020,
-  //         "valor": "R$ 56.000,000"
-  //       }
-  //     ]),
-  //   });
-  //   render(<ListagemVeiculos />);
-  //   const fiatText = await screen.findByText('HONDA');
-  //   fireEvent.click(fiatText);
-  //   const botaoExcluir = screen.getByTestId('botao-excluir');
-  //   fireEvent.click(botaoExcluir);
-  //   jest.spyOn(global, 'fetch').mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue([{
-  //       "id": 4,
-  //       "marca": "HYUNDAY",
-  //       "modelo": "HB20",
-  //       "ano": 2020,
-  //       "valor": "R$ 56.000,000"
-  //     }]),
-  //   });
-  //   expect(await screen.findByText('HONDA')).not.toBeInTheDocument();
-  // });
+    await act(async () => {
+      await render(<CadastroVeiculos />);
+    });
+
+    expect(true).toBe(true);
+
+    const inputs = [
+      {id: 'inputModelo', value: 'modelo'}, 
+      {id: 'inputAno', value: '2010'}, 
+      {id: 'inputValor', value: '20000' }
+    ];
+
+    inputs.forEach(async element => {
+      await changeInput(element.id, element.value);
+      const input = await screen.getByTestId(element.id).querySelector('input');
+      expect(input.value).toBe(element.value);
+    });
+  });
 });
