93d152fb12dc200175983744dc73fb81b48fceac
marcos <marcos_nto@hotmail.com>
2021-07-20 10:08:52 -0300

    verificacao de variaveis para direcionamento de rotas

--- src/pages/Marca/ListagemMarca/ListagemMarcas.jsx
+++ src/pages/Marca/ListagemMarca/ListagemMarcas.jsx
@@ -26,7 +26,10 @@ const ListagemMarcas = () => {
 
   return (
     <Listagem
-      alterar={() => history.push(`/alteracao-marca/${marcaSelecionada?.id}`)}
+      alterar={() => {
+        marcaSelecionada &&
+          history.push(`/alteracao-marca/${marcaSelecionada?.id}`);
+      }}
       excluir={excluir}
       incluir={() => history.push('/cadastro-marca')}
       colunas={colunas}
