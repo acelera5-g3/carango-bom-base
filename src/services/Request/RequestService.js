4ad9c513cdb1e43d23551fc73639476e70acafb9
Ary <arycleniobarros@gmail.com>
2021-07-20 12:23:35 -0300

    Criando env

---
+++ src/services/Request/RequestService.js
@@ -0,0 +1,15 @@
+const request = (url, method, body) => {
+  let headers = { 'Content-type': 'application/json' };
+  const token = localStorage.getItem('token');
+  if (token) {
+    headers = { ...headers, Authorization: `Bearer ${token}` };
+  }
+  return fetch(url, {
+    method,
+    body: JSON.stringify(body),
+    timeout: 5000,
+    headers,
+  });
+};
+
+export default request;
---
+++ src/services/Request/RequestService.test.js
@@ -0,0 +1,10 @@
+import request from './RequestService';
+
+describe('Request service', () => {
+  it('deve testar token em local storage', () => {
+    // jest
+    //   .spyOn(global.localStorage, 'getItem').mock;
+    // const req = request('http://test.com', 'METHOD', {});
+    expect(true).toBeTruthy();
+  });
+});
