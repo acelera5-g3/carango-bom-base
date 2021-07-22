12e0351f47fef5c0c969fbb2cbb2fcbb05101286
arycleniobarros <arycleniobarros@gmail.com>
2021-07-22 17:06:46 -0300

    Updating tests

--- src/pages/Usuarios/ListagemUsuario/ListagemUsuario.test.js
+++ src/pages/Usuarios/ListagemUsuario/ListagemUsuario.test.js
@@ -44,30 +44,30 @@ describe('ListagemUsuario', () => {
   //   expect(history.location.pathname).toBe('/alteracao-usuario/1');
   // });
 
-  // it('Deve excluir uma usuario', async () => {
-  //   jest
-  //     .spyOn(UsuarioService, 'excluir')
-  //     .mockResolvedValue({ id: 1, email: 'teste@teste.com' });
-  //   jest
-  //     .spyOn(UsuarioService, 'listar')
-  //     .mockClear()
-  //     .mockResolvedValueOnce({
-  //       content: [
-  //         { id: 1, email: 'teste@teste.com' },
-  //         { id: 2, email: 'teste2@teste.com' },
-  //       ],
-  //     })
-  //     .mockResolvedValue({ content: [{ id: 2, email: 'teste2@teste.com' }] });
-  //   createInstance();
+  it('Deve excluir uma usuario', async () => {
+    jest
+      .spyOn(UsuarioService, 'excluir')
+      .mockResolvedValue({ id: 1, email: 'teste@teste.com' });
+    jest
+      .spyOn(UsuarioService, 'listar')
+      .mockClear()
+      .mockResolvedValueOnce({
+        content: [
+          { id: 1, email: 'teste@teste.com' },
+          { id: 2, email: 'teste2@teste.com' },
+        ],
+      })
+      .mockResolvedValue({ content: [{ id: 2, email: 'teste2@teste.com' }] });
+    await render(<ListagemUsuario />);
 
-  //   const fiatText = await screen.findByText('teste@teste.com');
-  //   const botaoExcluir = screen.getByTestId('botao-excluir');
+    const fiatText = await screen.findByText('teste@teste.com');
+    const botaoExcluir = screen.getByTestId('botao-excluir');
 
-  //   fireEvent.click(fiatText);
-  //   fireEvent.click(botaoExcluir);
+    fireEvent.click(fiatText);
+    fireEvent.click(botaoExcluir);
 
-  //   expect(await screen.findByText('teste@teste.com')).not.toBeInTheDocument();
-  // });
+    expect(await screen.findByText('teste@teste.com')).not.toBeInTheDocument();
+  });
 
   it('Deve cadastrar uma usuario', async () => {
     createInstance();
