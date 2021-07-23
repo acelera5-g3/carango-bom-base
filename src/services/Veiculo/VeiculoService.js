3b1b215da77b9bebb4d78de2657bfd714f081401
Felipe Pasqualotto <felipe.psq@outlook.com>
2021-07-23 17:42:19 -0300

    testes para heroku

--- src/services/Veiculo/VeiculoService.js
+++ src/services/Veiculo/VeiculoService.js
@@ -3,7 +3,7 @@ import request from '../Request/Request';
 const VeiculoService = {
   cadastrar(veiculo) {
     return request(
-      `${process.env.REACT_APP_API_URL}/veiculos`,
+      `${process.env.REACT_APP_API_URL}/apiveiculos`,
       'POST',
       veiculo
     ).then((r) => r.json());
@@ -11,7 +11,7 @@ const VeiculoService = {
 
   alterar(veiculo) {
     return request(
-      `${process.env.REACT_APP_API_URL}/veiculos/${veiculo.id}`,
+      `${process.env.REACT_APP_API_URL}/apiveiculos/${veiculo.id}`,
       'PUT',
       veiculo
     ).then((r) => r.json());
@@ -19,20 +19,20 @@ const VeiculoService = {
 
   consultar(id) {
     return request(
-      `${process.env.REACT_APP_API_URL}/veiculos/${id}`,
+      `${process.env.REACT_APP_API_URL}/apiveiculos/${id}`,
       'GET'
     ).then((r) => r.json());
   },
 
   listar() {
-    return request(`${process.env.REACT_APP_API_URL}/veiculos`, 'GET').then(
+    return request(`${process.env.REACT_APP_API_URL}/apiveiculos`, 'GET').then(
       (r) => r.json()
     );
   },
 
   excluir(veiculo) {
     return request(
-      `${process.env.REACT_APP_API_URL}/veiculos/${veiculo.id}`,
+      `${process.env.REACT_APP_API_URL}/apiveiculos/${veiculo.id}`,
       'DELETE'
     ).then((r) => r.json());
   },
