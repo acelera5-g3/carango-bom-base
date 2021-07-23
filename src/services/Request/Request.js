9215ecb36ec7bc8a584f835d9525625bb0b29c36
Felipe Pasqualotto <felipe.psq@outlook.com>
2021-07-23 15:44:21 -0300

    testes no heroku

--- src/services/Request/Request.js
+++ src/services/Request/Request.js
@@ -7,13 +7,8 @@ const request = (url, method, body) => {
       Authorization: `Bearer ${token}`,
     };
   }
-  console.log({ url });
-  console.log({ method });
-  console.log({ body });
-  console.log({ headers });
   return fetch(url, {
     method,
-    mode: 'cors',
     body: JSON.stringify(body),
     timeout: 5000,
     headers,
